268c6359848e1d7d32dbb2684bb74bf93748bcb8
your name <eli.pousson@gmail.com>
2024-03-18 16:24:23 -0400

    Fix error in inline embedding logic

--- _extensions/d2/d2.lua
+++ _extensions/d2/d2.lua
@@ -245,7 +245,7 @@ local function render_graph(globalOptions)
           os.remove(outputPath)
           return data
         elseif options.embed_mode == EmbedMode.inline then
-          if not options.format == "pdf" then
+          if options.format ~= "pdf" then
             os.remove(outputPath)
             return "data:" .. mimetype .. ";base64," .. quarto.base64.encode(data)
           else
